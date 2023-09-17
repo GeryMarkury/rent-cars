5c10b4fb056a25889f1c2d75ee9f9574ac184fdd
Heorhii <gerythepatient@gmail.com>
2023-09-17 12:05:29 +0200

    edit: CarsList

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -1,35 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "../Button/Button";
 import css from "./CarCard.module.scss";
 import icons from '/src/images/icons.svg';
 
-export const CarCard = ({ obj, img, make, model, type, year, price, address, company, id, accessories }) => {
-    const [isInFavorites, setIsInFavorites] = useState(false);
-    const [carsInStorage, setCarsInStorage] = useState([]);
-    
-    const CARS_STORAGE = "cars";
-
-    useEffect(() => {
-    const carsDataJson = localStorage.getItem(CARS_STORAGE);
-      if (carsDataJson) {
-        setCarsInStorage(JSON.parse(carsDataJson))
-      }
-
-    carsInStorage.find(car => car.id === id) ? setIsInFavorites(true) : setIsInFavorites(false);
-    }, [carsDataJson])
+export const CarCard = ({ obj, img, make, model, type, year, price, address, company, id, accessories, isInFavorites, addToFavorites, removeFromFavorites }) => {
+    const [isFavorite, setIsFavorite] = useState(isInFavorites);
 
     const fullAddress = address.split(', ');
     const city = fullAddress[fullAddress.length - 2];
     const accessory = accessories[0];
 
-    const addToFavorites = (car) => {
-      setCarsInStorage(carsInStorage.push(car));
-      localStorage.setItem(CARS_STORAGE, JSON.stringify(carsInStorage));
+    const handleAddToFavorites = (car) => {
+      addToFavorites(car);
+      setIsFavorite(true);
     }
 
-    const removeFromFavorites = (car) => {
-      setCarsInStorage(carsInStorage.filter(item => item.id !== car.id));
-      localStorage.setItem(CARS_STORAGE, JSON.stringify(carsInStorage));
+    const handleRemoveFromFavorites = (car) => {
+      removeFromFavorites(car);
+      setIsFavorite(false);
     }
 
    const handleClick = () => {
@@ -39,7 +27,7 @@ export const CarCard = ({ obj, img, make, model, type, year, price, address, com
     return (
         <div className={css.container}>
             <img src={img} alt={make} className={css.img} />
-            {isInFavorites ? <button type="button" className={css.favoriteBtn} onClick={() => removeFromFavorites(obj)}><svg className={css.favoriteIconActive}><use href={icons + "#favorite"}></use></svg></button> : <button type="button" className={css.favoriteBtn} onClick={() => addToFavorites(obj)}><svg className={css.favoriteIcon}><use href={icons + "#favorite"}></use></svg></button>}
+            {isFavorite ? <button type="button" className={css.favoriteBtn} onClick={() => handleRemoveFromFavorites(obj)}><svg className={css.favoriteIconActive}><use href={icons + "#favorite"}></use></svg></button> : <button type="button" className={css.favoriteBtn} onClick={() => handleAddToFavorites(obj)}><svg className={css.favoriteIcon}><use href={icons + "#favorite"}></use></svg></button>}
             <div>
                 <div className={css.cardTitle}><p>{make} <span>{model}</span>, {year}</p>
                     <p>{price}</p></div>
--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -1,13 +1,41 @@
+import { useState, useEffect } from "react";
 import { CarCard } from "../CarCard/CarCard";
 import css from "./CarsList.module.scss";
 
 export const CarsList = ({ cars }) => {
+    const [carsInStorage, setCarsInStorage] = useState([]);
     
+    const CARS_STORAGE = "cars";
+
+    useEffect(() => {
+    const carsDataJson = localStorage.getItem(CARS_STORAGE);
+      if (carsDataJson) {
+        const storedCars = JSON.parse(carsDataJson);
+            setCarsInStorage(storedCars);
+      } else {
+        localStorage.setItem(CARS_STORAGE, "[]")
+      }
+    }, [])
+
+    const addToFavorites = (car) => {
+    const updatedCars = [...carsInStorage, car];
+    setCarsInStorage(updatedCars);
+    localStorage.setItem(CARS_STORAGE, JSON.stringify(updatedCars));
+    }
+    
+    const removeFromFavorites = (car) => {
+    const updatedCars = carsInStorage.filter(item => item.id !== car.id);
+    setCarsInStorage(updatedCars);
+    localStorage.setItem(CARS_STORAGE, JSON.stringify(updatedCars));
+  }
+
+
     return (
         <ul className={css.carsList}>
             {cars.map((car) => (
         <li key={car.id}>
-                    <CarCard obj={car} img={car.img} make={car.make} model={car.model} year={car.year} price={car.rentalPrice} address={car.address} company={car.rentalCompany} type={car.type} id={car.id} accessories={car.accessories} />
+                    <CarCard obj={car} img={car.img} make={car.make} model={car.model} year={car.year} price={car.rentalPrice} address={car.address} company={car.rentalCompany} type={car.type} id={car.id} accessories={car.accessories} isInFavorites={carsInStorage.some(item => item.id === car.id)} addToFavorites={addToFavorites}
+          removeFromFavorites={removeFromFavorites}/>
         </li>
       ))}
 </ul>
