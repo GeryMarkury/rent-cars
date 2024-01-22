c86db7e10141f4b2a92327fec1390344e33c6945
Heorhii <hmarkov.dev@gmail.com>
2024-01-22 18:31:39 +0100

    start Formik component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import cssBtn from "/src/components/Button/Button.module.scss";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 
 export const Sidebar = () => {
 	// const makes = ["Buick","Volvo","HUMMER","Subaru","Mitsubishi","Nissan","Lincoln","GMC","Hyundai","MINI","Bentley","Mercedes-Benz","Aston Martin","Pontiac","Lamborghini","Audi","BMW","Chevrolet","Mercedes-Benz","Chrysler","Kia","Land"]
 
-	const handleClick = () => {
-		console.log("Hi, I'm Button");
-	};
+	const [params, setParams] = useState("");
 
 	return (
-		<div>
+		<Formik
+			initialValues={{ makes: "", price: "", mileageFrom: "", mileageTo: "" }}
+			onSubmit={values => {
+				setParams({ ...params, ...values, page: 1 });
+			}}
+		>
 			<Button
 				type="submit"
 				title="Search"
-				onClick={handleClick}
 				propClass={cssBtn.searchBtn}
 			/>
-		</div>
+		</Formik>
 	);
 };
